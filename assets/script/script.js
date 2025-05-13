22346e530470d88166fe2fe62fd5219c8577bec0
Calebe <calebec055@gmail.com>
2025-05-13 18:27:19 +0100

    Melhorias no menu

---
+++ assets/script/script.js
@@ -0,0 +1,5 @@
+document.querySelector(".barMenu").addEventListener("click", ()=> {
+    document.querySelector(".menuMobile").classList.add("translate-x-0")
+    console.log("ENTROUUUUU")
+                                    
+})
